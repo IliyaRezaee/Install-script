1cb051faf57f17867e726b00b07294f42773eb1c
Iliya <69698913+IliyaRezaee@users.noreply.github.com>
2020-08-15 11:31:01 +0430

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -3,8 +3,7 @@ sudo apt-get upgrade
 sudo apt install git
 sudo apt install vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir /tmp/vim-config
-git clone https://github.com/IliyaRezaee/vim-config.git /tmp/vim-config/
+git clone https://github.com/IliyaRezaee/vim-config.git /tmp
 cp /tmp/vim-config/vimrc.txt ~/.vimrc
 sudo apt install xfce4-terminal
 sudo apt install zsh
