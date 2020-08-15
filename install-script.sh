b32870dbe3f39c0ad144a38cd4e7320c6e39834f
iliya <iliyarezaee2017@gmail.com>
2020-08-15 11:15:53 +0430

    Create and write "install-script.sh"

---
+++ install-script.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir /tmp/vim-config
+git clone https://github.com/IliyaRezaee/vim-config.git /tmp/vim-config/
+cp /tmp/vim-config/vimrc.txt ~/.vimrc
+sudo apt install xfce4-terminal
+sudo apt install zsh
+chsh -s /usr/bin/zsh
