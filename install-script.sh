e75ae535f6210ba361f3141d5b022d65c68f9257
Iliya <69698913+IliyaRezaee@users.noreply.github.com>
2020-08-16 23:49:53 +0430

    install oh-my-zsh theme for zsh shell

--- install-script.sh
+++ install-script.sh
@@ -6,6 +6,7 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/IliyaRezaee/vim-config.git /tmp
 cp /tmp/vim-config/vimrc.txt ~/.vimrc
 sudo apt install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sudo chsh -s /usr/bin/zsh
 #########################
 sudo apt install jcal
