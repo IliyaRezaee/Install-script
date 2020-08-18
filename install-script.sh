0cd47d2bbe6f2da5c374987e923f2d07505f7769
iliya <iliyarezaee2017@gmail.com>
2020-08-18 20:11:00 +0430

    Update file

--- install-script.sh
+++ install-script.sh
@@ -3,10 +3,9 @@ sudo apt-get upgrade
 sudo apt install git
 sudo apt install vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/IliyaRezaee/vim-config.git /tmp
-cp /tmp/vim-config/vimrc.txt ~/.vimrc
+cp vimrc.txt ~/.vimrc
 sudo apt install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sudo chsh -s /usr/bin/zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 #########################
 sudo apt install jcal
