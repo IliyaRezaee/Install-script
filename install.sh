10be68d8faef332392898fee55dabb962cc671c8
Iliya <69698913+IliyaRezaee@users.noreply.github.com>
2020-08-29 19:41:03 +0430

    install curl

--- install.sh
+++ install.sh
@@ -33,7 +33,8 @@ sudo chsh -s /usr/bin/zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "=============End of installing and configuration of 'zsh'...============="
 
-echo "=============[Installing 'jcal, screenfetch'](option)============="
+echo "=============Installing others============="
 sudo apt install jcal
 sudo apt install screenfetch
+sudo apt install curl
 echo "Done!"
